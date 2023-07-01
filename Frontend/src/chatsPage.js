16a9cb1fc82751d50e585589ebe5d9327d6ea3c5
farzana <farzanach025@gmail.com>
2023-07-01 17:31:33 -0600

    multi chat window added

--- Frontend/src/chatsPage.js
+++ Frontend/src/chatsPage.js
@@ -1,6 +1,11 @@
-import { PrettyChatWindow } from "react-chat-engine-pretty";
+//import { PrettyChatWindow } from "react-chat-engine-pretty";
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
 
-const ChatsPage = (props) => {
+/* const ChatsPage = (props) => {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <PrettyChatWindow
@@ -9,6 +14,24 @@ const ChatsPage = (props) => {
         secret={props.user.secret} 
         style={{ height: "100%" }}
       />
+    </div>
+  );
+};
+
+export default ChatsPage; */
+
+const ChatsPage = (props) => {
+  console.log("props", props);
+  const chatProps = useMultiChatLogic(
+    process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID,
+    props.user.username,
+    props.user.secret
+  );
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
     </div>
   );
 };
